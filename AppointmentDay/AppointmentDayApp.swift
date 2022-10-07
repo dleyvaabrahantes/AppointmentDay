8b117673dc3d8a4a8665134825341bcb79809554
David Leyva <dleyvaabrahantes@gmail.com>
2022-10-07 11:01:00 -0400

    Initial Commit

---
+++ AppointmentDay/AppointmentDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppointmentDayApp.swift
+//  AppointmentDay
+//
+//  Created by David on 10/7/22.
+//
+
+import SwiftUI
+
+@main
+struct AppointmentDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
