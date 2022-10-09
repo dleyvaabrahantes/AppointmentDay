afe6676ffa66d652eadde8ed8f793052d016ac2e
David Leyva <dleyvaabrahantes@gmail.com>
2022-10-08 22:39:18 -0400

    [Add]- Add Button float

--- AppointmentDay/ContentView.swift
+++ AppointmentDay/ContentView.swift
@@ -8,13 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var show: Bool = false
     var body: some View {
-        Home()
+        ZStack{
+            Home()
+            ButtonFloat(show: $show)
+            
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+struct ButtonFloat:View {
+    @Binding var show: Bool
+    var body: some View{
+        VStack{
+            Spacer()
+            HStack{
+                Spacer()
+                Button {
+                    self.show.toggle()
+                } label: {
+                    Image(systemName: "plus")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 25, height: 15)
+                        .padding(22)
+                    
+                }
+                .background(Color.black)
+                .foregroundColor(.white)
+                .clipShape(Circle())
+                .rotationEffect(.init(degrees: show ? 180 : 0))
+
+            }
+            .animation(.spring())
+        }
     }
 }
